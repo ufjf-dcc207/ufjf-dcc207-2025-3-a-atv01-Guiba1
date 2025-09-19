f4e8eaa5412490c607e8be9753f5b2f6e863e321
Guilherme Tavares de Freitas <tavares.guilherme@estudante.ufjf.br>
2025-09-18 22:28:43 -0300

    :sparkles: adciona gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/front end/ufjf-dcc207-2025-3-a-atv01-Guiba1",
   plugins: [react()],
 })
